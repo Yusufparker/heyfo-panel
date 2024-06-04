2cb3ebb30342f7d8217cc55edcc9d1986d95296a
Muhammad Yusuf <mrmy160602@gmail.com>
2024-06-04 13:46:52 +0700

    update

--- src/resources/views/Auth/login.blade.php
+++ src/resources/views/Auth/login.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <link href="https://heyfo-6ppaqiiwua-et.a.run.app/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Login | HeyFo</title>
 </head>
 <body>
--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{asset('css/app.css/') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     {{-- <link rel="shortcut icon" href="{{asset('img/logo-health.png')}}" type="image/x-icon"> --}}
-    {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
     <script src="https://kit.fontawesome.com/53c338f417.js" crossorigin="anonymous"></script>
     <title>Dashboard | e-health</title>
 </head>
