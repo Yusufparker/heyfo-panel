4ec957a6747e35e84b84bfddce8832b8c6d784b0
Muhammad Yusuf <mrmy160602@gmail.com>
2024-06-04 13:05:16 +0700

    update

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{'css/app.css/') }}" rel="stylesheet">
     {{-- <link rel="shortcut icon" href="{{asset('img/logo-health.png')}}" type="image/x-icon"> --}}
     <script src="https://kit.fontawesome.com/53c338f417.js" crossorigin="anonymous"></script>
     <title>Dashboard | e-health</title>
